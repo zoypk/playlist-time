3a4eab8581bfec8b74bb5cd9db5a26c41f8d2580
Mohammed Zohil PK <44064613+zoypk@users.noreply.github.com>
2026-02-24 13:58:13 +0530

    feat: update onRequest function to handle routing based on request pathname

--- functions/index.ts
+++ functions/index.ts
@@ -1,6 +1,12 @@
-export async function onRequest() {
-  return new Response("OK", {
-    status: 200,
-    headers: { "Content-Type": "text/plain" },
-  });
+export async function onRequest({ request }: { request: Request }) {
+  const { pathname } = new URL(request.url);
+
+  if (pathname === "/ping") {
+    return new Response("OK", {
+      status: 200,
+      headers: { "Content-Type": "text/plain" },
+    });
+  }
+
+  return new Response("Not Found", { status: 404 });
 }
