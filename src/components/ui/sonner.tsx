88cd561f7c4d8a520afdf4e0c35ef48d4dfa9c3b
Mohammed Zohil PK <44064613+zoypk@users.noreply.github.com>
2026-02-25 14:28:49 +0530

    feat: add closeButton option to Toaster component for improved user interaction

--- src/components/ui/sonner.tsx
+++ src/components/ui/sonner.tsx
@@ -8,6 +8,7 @@ const Toaster = ({ ...props }: ToasterProps) => {
       theme="dark"
       className="toaster group"
       toastOptions={{
+        closeButton: true,
         classNames: {
           toast:
             "group toast group-[.toaster]:bg-background group-[.toaster]:text-foreground group-[.toaster]:border-border group-[.toaster]:shadow-lg",
